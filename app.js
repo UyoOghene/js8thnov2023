7185dd8b5dd0be8dded850bcc8dc91c3a4a4ebe0
Uyooghene <uyooghene01@gmail.com>
2023-11-09 23:58:10 -0800

    functions9th nov

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// function getCard(){
+//     const valucard =['1','2','3','4','5','6','7','8','9','10','j','k','q','a'];
+//     const keycard =['clubs','hearts','spades','diamonds'];
+//    const validx = Math.floor(Math.random()*valucard.length);
+//    const values= valucard[validx] ;
+//    const keyidx=Math.floor(Math.random()*keycard.length);
+//     const keys=keycard[keyidx]
+//    return {values:values, keys:keys};
+
+// }
+
+// getCard()
+
+// alternatively
+// create a function to get a random value from any array
+
+// function pick(arr){
+//    const idx= Math.floor(Math.random()*arr.length);
+//    console.log(arr[idx]);
+// }
+// function getCard(){
+//     const valucard =['1','2','3','4','5','6','7','8','9','10','j','k','q','a'];
+//     const keycard =['clubs','hearts','spades','diamonds'];
+// return {values:pick(valucard), keys:pick(keycard)};
+// }
+// getCard()
