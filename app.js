e3e2d4ea05a3ca62c9377d26080f567193ee1e1f
Uyooghene <uyooghene01@gmail.com>
2023-11-10 04:12:29 -0800

    update

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 
-// function getCard(){
-//     const valucard =['1','2','3','4','5','6','7','8','9','10','j','k','q','a'];
-//     const keycard =['clubs','hearts','spades','diamonds'];
-//    const validx = Math.floor(Math.random()*valucard.length);
-//    const values= valucard[validx] ;
-//    const keyidx=Math.floor(Math.random()*keycard.length);
-//     const keys=keycard[keyidx]
-//    return {values:values, keys:keys};
+function getCards(){
+    const valucard =['1','2','3','4','5','6','7','8','9','10','j','k','q','a'];
+    const keycard =['clubs','hearts','spades','diamonds'];
+   const validx = Math.floor(Math.random()*valucard.length);
+   const values= valucard[validx] ;
+   const keyidx=Math.floor(Math.random()*keycard.length);
+    const keys=keycard[keyidx]
+   return {values:values, keys:keys};
 
-// }
+}
 
-// getCard()
+console.log(getCards())
 
 // alternatively
 // create a function to get a random value from any array
 
-// function pick(arr){
-//    const idx= Math.floor(Math.random()*arr.length);
-//    console.log(arr[idx]);
-// }
-// function getCard(){
-//     const valucard =['1','2','3','4','5','6','7','8','9','10','j','k','q','a'];
-//     const keycard =['clubs','hearts','spades','diamonds'];
-// return {values:pick(valucard), keys:pick(keycard)};
-// }
-// getCard()
+function pick(arr){
+   const idx= Math.floor(Math.random()*arr.length);
+   return arr[idx];
+}
+function getCard(){
+    const valucard =['1','2','3','4','5','6','7','8','9','10','j','k','q','a'];
+    const keycard =['clubs','hearts','spades','diamonds'];
+return {values:pick(valucard), keys:pick(keycard)};
+}
+console.log(getCard())
+
